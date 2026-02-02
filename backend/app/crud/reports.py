c62e89df38026477462f525fb097c5a349fbe56f
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-02-02 12:04:23 -0500

    base

---
+++ backend/app/crud/reports.py
@@ -0,0 +1,29 @@
+import uuid
+from sqlalchemy.orm import Session
+from models.user import user
+from schemas.user import UserCreate
+
+
+def create_report():
+    pass
+
+
+def get_report():
+    pass
+
+
+def get_report_by_user_id():
+    pass
+
+
+def get_report_by_email():
+    pass
+
+
+def get_reports():
+    pass
+
+
+# Presumably delete the report once the infraction has been handled
+def delet_report():
+    pass
