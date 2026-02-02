7941214d77743dec47ba470f0ab9197b0ebe10f1
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-02-02 17:19:08 -0500

    another small fix

--- backend/app/models/reports.py
+++ backend/app/models/reports.py
@@ -5,7 +5,9 @@ from sqlalchemy import String, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
-from app.db.base import Base
+
+
+from backend.app.db.base import Base
 
 class Reports(Base):
    __tablename__ = "reports"
@@ -20,9 +22,9 @@ class Reports(Base):
     # Takes the user_id from user model
 
     user_id:Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),ForeignKey("user.user_id"), nullable=False)
-    # Report ID 
+    # Report ID
     report_id:Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, defualt=uuid.uuid4)
-    # Description ie contents of the report 
+    # Description ie contents of the report
     description:Mapped[str] = mapped_column(String(1000), nullable=False)
     # Time stamp of when the person/listing was reported
     created_at:Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
