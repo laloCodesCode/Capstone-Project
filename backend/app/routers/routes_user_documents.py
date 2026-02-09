65adbdf1b97720ca895a19a462842167b5b51eb9
lilynguyen46 <lilygriggs46@gmail.com>
2026-02-09 15:52:41 -0500

    User doc route fix

---
+++ backend/app/routers/routes_user_documents.py
@@ -0,0 +1,38 @@
+import uuid 
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from crud import user_documents_crud as crud_user_documents
+from schemas.user_documents import User_DocumentsCreate, User_DocumentsBase, User_documentsUpdate, User_DocumentOut
+from db.session import get_db
+
+
+router = APIRouter(prefix="/user-documents", tags=["user-documents"])
+
+
+# Route to create new user document tied with a user
+@router.post("/", response_model=User_DocumentOut)
+def create_user_document_endpoint(user_document: User_DocumentsCreate, db: Session = Depends(get_db), current_user: uuid.UUID = Depends(crud_user_documents.get_current_user)):
+    return crud_user_documents.create_user_document(db, user_document)  
+
+
+# Route to read a user doc by uts uuid
+@router.get("/{user_document_id}", response_model=User_DocumentOut)
+def read_user_document(user_document_id: uuid.UUID, db: Session = Depends(get_db)):
+    db_user_document = crud_user_documents.get_user_document(db, user_document_id)
+    if not db_user_document:
+        raise HTTPException(status_code=404, detail="User document not found!")
+    return db_user_document
+
+
+# Route to update a user doc by its uuid
+@router.put("/{user_document_id}", response_model=User_DocumentOut)
+def update_user_document_endpoint(user_document_id: uuid.UUID, user_document: User_documentsUpdate, db: Session = Depends(get_db)):
+    db_user_document = crud_user_documents.get_document(db, user_document_id)
+
+    if not db_user_document:
+        raise HTTPException(status_code=404, detail="User document not found!")
+    
+    if db_user_document.user_id != user_document.user_id:
+        raise HTTPException(status_code=403, detail="Not authorized to update this user document!")
+    
+    return crud_user_documents.update_user_document(db, user_document=db_user_document, user_document_in= user_document)
