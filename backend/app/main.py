dc292a5aa4b45ebaac5d51cfc6e93c5f8684877a
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-02-09 15:59:31 -0500

    called routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+
 from routers import routes_user
 from routers import routes_reports
+from routers import routes_user_documents
+from routers import routes_item_listing
+
+
+
+
+
 from db.base import Base
 from db.session import engine
 
@@ -26,3 +34,5 @@ def health():
 app.include_router(user.router)
 #including all the routers for the reports
 app.include_router(reports.router)
+app.include_router(routes_user_documents.router)
+app.include_router(routes_item_listing.router)
