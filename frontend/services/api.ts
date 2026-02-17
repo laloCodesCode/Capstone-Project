407eae88d0213aad84f3896500a9f29eb472d36a
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-02-17 11:48:30 -0500

    started api calls for the frontend, BROKEN

---
+++ frontend/services/api.ts
@@ -0,0 +1,38 @@
+
+
+
+
+// Base URL (suggested set up)
+
+
+// Base URL var, need to setup some environmental vars
+const BASE_URL;
+
+
+
+
+
+//exception handling 
+if (!BASE_URL) {
+  throw new Error("BASE_URL not configured properly!")
+}
+
+
+
+
+//Setup a generic function for api calls 
+export async function apiFetch<T>(endpoin: string, options? RequestInit) Promise < T > {
+
+
+
+
+  // API BODY
+
+
+  //TODO figure it out!
+
+
+
+
+
+}
