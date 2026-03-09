767b57f5f9a51e12e5f1a52449c30d11d15ad397
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-03-09 11:32:46 -0400

    added some basic comments to help with the undesrstanding of the code.

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,14 +1,18 @@
 import * as SecureStore from "expo-secure-store";
 import { UserCreate, UserResponse, TokenResponse } from "../types/auth";
 
+// Untracked env vars this uses my PERSONAL IP
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
+//Takes in  email or username and a password to create the bearer token
 export const authService = {
+  // LOGIN LOGIC
   login: async (identifier: string, password: string): Promise<string> => {
     const formData = new URLSearchParams();
+
+    // NOT A JSON obeject, x-www-form-urlencoded
     formData.append("username", identifier);
     formData.append("password", password);
-
     const res = await fetch(`${BASE_URL}/token`, {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -25,7 +29,9 @@ export const authService = {
     return data.access_token;
   },
 
+  // REGISTER LOGIC
   register: async (userData: UserCreate): Promise<UserResponse> => {
+    // Register USES JSON objects
     const res = await fetch(`${BASE_URL}/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -40,10 +46,12 @@ export const authService = {
     return res.json() as Promise<UserResponse>;
   },
 
+  // Deletes the cached token
   logout: async (): Promise<void> => {
     await SecureStore.deleteItemAsync("token");
   },
 
+  //Token, hinges on prevoius login to have user logged in on app launch
   getToken: async (): Promise<string | null> => {
     return await SecureStore.getItemAsync("token");
   },
