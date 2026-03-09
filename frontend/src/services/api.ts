a6b827f552bba3c199c70e1b51321ce2a7c44eee
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-03-09 12:03:20 -0400

    api service for token handling

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,38 @@
 import axios from "axios";
-import { ItemListing } from "../types/item_listing";
+import * as SecureStore from "expo-secure-store";
+// import { ItemListing } from "../types/item_listing";
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8000", 
-    timeout: 10000,
-  });
+  //Use the env specific to your local environment
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  timeout: 10000,
+});
 
-  export const fetchItemListings = async (): Promise<ItemListing[]> => {
-    try {
-      const response = await api.get<ItemListing[]>("/items/");
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching item listings:", error);
-      throw error;
+// export const fetchItemListings = async (): Promise<ItemListing[]> => {
+//   try {
+//     const response = await api.get<ItemListing[]>("/items/");
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching item listings:", error);
+//     throw error;
+//   }
+// };
+
+// attach the bearer token to the every request
+api.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+// handel the token expiration
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await SecureStore.deleteItemAsync("token");
     }
-  };
+    return Promise.reject(error);
+  },
+);
