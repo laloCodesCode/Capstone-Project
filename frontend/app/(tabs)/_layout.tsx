983f0148011e6e955c71346516cdb755e2082634
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-03-10 18:38:26 -0400

    fixed

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ export default function TabsLayout() {
       <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="inbox" options={{ title: "Inbox" }} />
       <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen name="post" options={{ title: "Post" }} />
     </Tabs>
   );
 }
