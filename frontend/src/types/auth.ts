99da1ed3603aeb01a98dd4d093e42c14320b100d
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-03-10 10:29:22 -0400

    added types for profile response

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,23 +1,26 @@
-// These analogous to my schemas in FastAPI
-export interface UserCreate{
-    first_name: string;
-    last_name: string;
-    email: string;
-    username: string
-    password: string;
+// interfaces for login, registration and token generation
+export interface UserCreate {
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+  password: string;
 }
-export interface UserResponse{
-    user_id: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    username: string;
-    role: string;
+export interface UserResponse {
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+  role: string;
 }
-export interface TokenResponse{
-    access_token: string;
-    token_type: string;
+export interface TokenResponse {
+  access_token: string;
+  token_type: string;
 }
-export interface AuthUser{
-    token: string;
+export interface AuthUser {
+  token: string;
 }
+
+// interface for user profile
+export interface MeResponse {}
