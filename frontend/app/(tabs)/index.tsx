366454e3ca5b7ec5b6b4a17bee670d64fa694705
lilynguyen46 <lilygriggs46@gmail.com>
2026-03-10 17:45:30 -0400

    eleted frontend folder before megre

--- frontend/app/(tabs)/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Text, View } from "react-native";
-import ItemListingScreen from "../../src/components/item_listing";
-
-
-export default function HomeScreen() {
-  return <ItemListingScreen />;
-}
