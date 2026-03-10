c42956aa5d3c62a298c24991ae21230245f608e8
lilynguyen46 <lilygriggs46@gmail.com>
2026-03-10 17:22:57 -0400

    Any last chnages:

--- backend/app/schemas/item_listing.py
+++ backend/app/schemas/item_listing.py
@@ -38,7 +38,7 @@ class Item_ListingOut(Item_ListingBase):
     """ API returns"""
     model_config = ConfigDict(from_attributes=True)
 
-    id: UUID
+    item_listing_id: UUID
     user_id: UUID
 
     is_active: bool
