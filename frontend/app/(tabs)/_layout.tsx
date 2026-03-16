d848159003028017bfe15936e8910ce3cb779911
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-03-15 20:55:06 -0400

    fixed the header and footer

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -5,46 +5,50 @@ import { Ionicons } from "@expo/vector-icons";
 import { colors } from "../../src/styles/colors";
 
 export default function TabsLayout() {
-
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: colors.primary01,
+          borderTopColor: colors.primary01,
+        },
+        tabBarActiveTintColor: colors.primary02,
+        tabBarInactiveTintColor: colors.registerColor,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, size, focused }) => (
+          tabBarIcon: ({ color, size }) => (
             <Ionicons name="home" size={size} color={color} />
           ),
         }}
       />
-
       <Tabs.Screen
         name="post"
         options={{
           title: "Post",
-          tabBarIcon: ({ color, size, focused }) => (
+          tabBarIcon: ({ color, size }) => (
             <Ionicons name="add-circle" size={size} color={color} />
           ),
         }}
       />
-
-
-
       <Tabs.Screen
         name="inbox"
         options={{
           title: "Inbox",
-          tabBarIcon: ({ color, size, focused }) => (
+          tabBarIcon: ({ color, size }) => (
             <Ionicons name="mail-open" size={size} color={color} />
           ),
         }}
       />
-
       <Tabs.Screen
         name="profile"
         options={{
           title: "User",
-          tabBarIcon: ({ color, size, focused }) => (
+          tabBarIcon: ({ color, size }) => (
             <Ionicons name="person" size={size} color={color} />
           ),
         }}
